9ab2993336604eb8e7d0fc2f82ecdc79bfc31bb9
seregsagapitov <seregsagapitov@gmail.com>
2023-11-29 01:45:43 +0700

    29/11/2023

--- src/main/java/com/seregsagapitov/autobase/repositories/AutoRepository.java
+++ src/main/java/com/seregsagapitov/autobase/repositories/AutoRepository.java
@@ -2,12 +2,13 @@ package com.seregsagapitov.autobase.repositories;
 
 import com.seregsagapitov.autobase.entities.Auto;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 
 
 @Repository
-public interface AutoRepository extends CrudRepository<Auto, Long> {
+public interface AutoRepository extends PagingAndSortingRepository<Auto, Long> {
     //List<Auto> findByModel_id(Long id);
     //Auto findOneByTitle(String title);
     //Auto findAutoByTrademarkTitle_trademark(String title);
